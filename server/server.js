43e3ee4428354a8adb2312c0ef53ca5977dd4dd8
gsion <gsion@163.com>
2015-09-22 10:26:48 +0800

    token登录，  目前改为 cookie 登录

--- server/server.js
+++ server/server.js
@@ -48,9 +48,6 @@ app.use(session({
 
 app.use(function(req, res, next) {
     req.db = db;
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
     res.set({"X-Powered-By":"Gsion"});
     next();
 });
